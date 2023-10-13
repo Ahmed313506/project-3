58f70596752cbdba5f07c7c9f55d8f9ebe7015b0
Ahmed313506 <75357356+Ahmed313506@users.noreply.github.com>
2023-10-14 00:37:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-// $(".news").click(function(){
-// $("body").css("color","red");   
-// })
-// document.addEventListener("click",function () { 
-//     document.querySelector(".news").classList("white");
-// })
+
 document.querySelectorAll(".news").forEach(function(element) {
     element.addEventListener("mouseover", function() {
         this.classList.add("white");
@@ -14,7 +9,7 @@ document.querySelectorAll(".news").forEach(function(element) {
         this.classList.remove("white");
     });
 });
-// alert("hello");
+
 document.querySelector(".readmore").addEventListener("mouseover", function() {
     this.style.backgroundColor = "whitesmoke"; 
     this.style.color = "black"; 
@@ -36,45 +31,6 @@ document.querySelectorAll(".thirdlist h3").forEach(function(element) {
         this.classList.remove("text");
     });
 });
-
-
-
-    // $("h3").mouseover(function () { 
-    //     $(this).addClass("text");
-    // });
-
-    // $("h3").mouseout(function () { 
-    //     $(this).removeClass("text");
-    // });
-
-    // document.querySelectorAll(".secondlist h3").forEach(function(element) {
-    //     element.addEventListener("mouseover", function() {
-    //         this.style.color="orange";
-    //         this.classList.add("text");
-    //     });
-    // });
-    // document.querySelectorAll(".secondlist h3").forEach(function(element) {
-    //     element.addEventListener("mouseout", function() {
-    //         this.style.color="black";
-    //         this.classList.remove("text");
-    //     });
-    // });
-    
-
-    
-    // $(".png6").mouseover(function () { 
-    //     // var categories = ['Wirtschaft','Wissenschaft','Politik','Leben','Kultur','Geschichte']
-    //     // var option ="";
-    //     // for (var i=0; i<categories.length;i++){
-    //     //     option+= '<option value="' +month[i] + ' " >' +month[i] +" </option>" 
-    //     // }
-    //     //     document.getElementById('month').innerHTML=option;
-    //         alert("hey");
-       
-        
-    // });
-  
-    
 
    $(".secondlist h3").on("mouseover", function() {
     $(this).css("color", "orange");
@@ -98,22 +54,11 @@ $(".flex1").on("mouseout", function() {
     $(this).removeClass("text");
 });
 
-// const returnHomeButton = document.querySelector(".Name2");
 
 document.querySelector(".Name").addEventListener("click", function () {
     location.reload();
  
 });
-
-
-// document.querySelector(".Name").addEventListener("mouseover", function () {
-//     // location.reload();
-//     this.style.backgroundColor="whitesmoke";
-// });
-
-// document.querySelector(".Name").addEventListener("mouseout", function () {
-//     this.style.backgroundColor="white";
-// });
 
 const dropdowns= document.querySelectorAll('.dropdown');
 
@@ -124,29 +69,19 @@ dropdowns.forEach(dropdown => {
     const options = dropdown.querySelectorAll('.menu2 li');
     const selected = dropdown.querySelector('.selected');
 
-    // Add event listeners or perform actions with the selected elements
     select.addEventListener('mouseover', () => {
-        // Perform an action when the se lect element is clicked.
+      
         select.classList.add('select-clicked');
         caret.classList.add('caret-rotate');
         menu2.classList.add('menu2-open');
 
     });
 
-   
-
-
-
-
-    // Add event listeners or perform actions with other elements if needed.
     options.forEach(option => {
         option.addEventListener('click', () => {
-            // selected.innerText = option.innerText;
             select.classList.remove('select-clicked');
             caret.classList.remove('caret-rotate');
             menu2.classList.remove('menu2-open');
-            
-            // Inside this block, you want to remove the 'active' class from other options, so it should be here
             options.forEach(option => {
                 option.classList.remove('active');
             });
