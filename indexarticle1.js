b9a895f67b254c93af620c1cd1bef034eb6d1ec8
Ahmed313506 <75357356+Ahmed313506@users.noreply.github.com>
2023-10-14 05:36:19 +0200

    Add files via upload

---
+++ indexarticle1.js
@@ -0,0 +1,80 @@
+
+document.querySelectorAll(".news").forEach(function(element) {
+    element.addEventListener("mouseover", function() {
+        this.classList.add("white");
+    });
+});
+document.querySelectorAll(".news").forEach(function(element) {
+    element.addEventListener("mouseout", function() {
+        this.classList.remove("white");
+    });
+});
+
+// document.querySelectorAll(".thirdlist h3").forEach(function(element) {
+//     element.addEventListener("mouseover", function() {
+//         this.style.color="orange";
+//         this.classList.add("text");
+//     });
+// });
+// document.querySelectorAll(".thirdlist h3").forEach(function(element) {
+//     element.addEventListener("mouseout", function() {
+//         this.style.color="white";
+//         this.classList.remove("text");
+//     });
+// });
+
+
+
+// $("a").on("mouseover", function() {
+//     $(this).css("color", "orange");
+//     $(this).addClass("text");
+// });
+
+
+// $("a").on("mouseout", function() {
+//     $(this).css("color", "black");
+//     $(this).removeClass("text");
+// });
+
+
+document.querySelector(".Name").addEventListener("click", function () {
+    location.href="index.html";
+ 
+});
+
+const dropdowns= document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu2 = dropdown.querySelector('.menu2');
+    const options = dropdown.querySelectorAll('.menu2 li');
+    const selected = dropdown.querySelector('.selected');
+
+    select.addEventListener('mouseover', () => {
+      
+        select.classList.add('select-clicked');
+        caret.classList.add('caret-rotate');
+        menu2.classList.add('menu2-open');
+
+    });
+
+    options.forEach(option => {
+        option.addEventListener('click', () => {
+            select.classList.remove('select-clicked');
+            caret.classList.remove('caret-rotate');
+            menu2.classList.remove('menu2-open');
+            options.forEach(option => {
+                option.classList.remove('active');
+            });
+         
+        });
+        option.classList.add('active');
+        $(document).click(function () { 
+            $('.select').removeClass('select-clicked');
+            $('.caret').removeClass('caret-rotate');
+            $('.menu2').removeClass('menu2-open');
+        });
+    });
+});
+
